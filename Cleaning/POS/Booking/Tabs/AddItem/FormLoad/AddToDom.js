e0f12c37e36eefe7fb4506e0c67e5cce51680e89
keshavsoft <nknnkumar@yahoo.com>
2023-05-01 11:51:01 +0530

    keshav

--- Cleaning/POS/Booking/Tabs/AddItem/FormLoad/AddToDom.js
+++ Cleaning/POS/Booking/Tabs/AddItem/FormLoad/AddToDom.js
@@ -1,9 +1,6 @@
 import { StartFunc as StartFuncWashTypeFill } from "./FillSelect/WashTypeFill.js";
 import { StartFunc as StartFillSelect } from "./FillSelect/MenTab/MenItems.js";
-// import { StartFunc as StartFuncToDomToOrderItems } from "./ToDom/ToTable/ToOrderItems.js";
 import { StartFunc as StartFuncToDomToOrderItems } from "../ToDom/ToTable/ToOrderItems.js";
-// import { StartFunc as StartFuncToAddOns } from "../ToDom/ToTable/ToAddOns.js";
-//import { StartFunc as StartFuncOrderItemsTableRow } from "../../FetchFuncs/FetchHtml/OrderItemsTableRow.js";
 import { StartFunc as StartFuncOrderItemsTableRow } from "../ToDom/Templates/OrderItemsTableRow.js";
 
 let StartFunc = () => {
